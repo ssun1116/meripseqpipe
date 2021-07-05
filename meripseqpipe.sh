7a962078f34471827485defa85ad5fb970de8be9
Seoyeon Kim <syk991116@korea.ac.kr>
2021-07-06 03:04:44 +0900

    Update nextflow options (cpu, mem, strand)

--- meripseqpipe.sh
+++ meripseqpipe.sh
@@ -15,7 +15,7 @@ nextflow run meripseqpipe -profile test,docker
 
 ## Nextflow analysis 
 nextflow run meripseqpipe -profile docker --designfile designfile2.tsv --comparefile compare.txt \
---fasta GRCh38.primary_assembly.genome.fa --gtf gencode.v38.primary_assembly.annotation.gtf --stranded yes
+--fasta GRCh38.primary_assembly.genome.fa --gtf gencode.v38.primary_assembly.annotation.gtf --max_cpus 6 --max_memory 32 --stranded yes
 
 ## designfile.tsv -> written with Rstudio 
 ## write.table(file, "filename", sep = '\t', row.names = F)
